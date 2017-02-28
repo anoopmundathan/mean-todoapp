bf6c1d905b9148450a696d4407cbe9407b131cd1
anoopmundathan <anoop.mundathan@gmail.com>
2017-02-28 06:41:05 +0000

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var express = require('express');
+
+var app = express();
+
+/**
+ * Serve static files from public folder
+ */
+app.use('/', express.static('public'));
+
+/**
+ * Set port
+ */
+app.set('port', process.env.PORT || 3000);
+
+/**
+ * Listen for incoming requests
+ */
+app.listen(app.get('port'), function() {
+	console.log('Server running at port', app.get('port'));
+});
