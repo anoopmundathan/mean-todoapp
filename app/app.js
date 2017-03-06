8ba7a53d9aa2c8f7384ee1f9e8e62b4d3e07c73f
anoopmundathan <anoop.mundathan@gmail.com>
2017-03-06 06:17:04 +0000

    Add data using ng-model

--- app/app.js
+++ app/app.js
@@ -2,15 +2,4 @@ var angular = require('angular');
 
 angular.module('todoListApp', [])
 	.controller('mainCtrl', function($scope) {
-		$scope.helloWorld = function() {
-			console.log('Hello there, This is from mainCtrl');
-		}
-	})
-	.controller('secondCtrl', function($scope) {
-		$scope.secondHello = function() {
-
-		}
-	})
-	.controller('siblingCtrl', function($scope) {
-		$scope.hi = "Sibling Controller";
 	});
--- public/scripts/bundle.js
+++ public/scripts/bundle.js
@@ -33221,17 +33221,6 @@ var angular = __webpack_require__(0);
 
 angular.module('todoListApp', [])
 	.controller('mainCtrl', function($scope) {
-		$scope.helloWorld = function() {
-			console.log('Hello there, This is from mainCtrl');
-		}
-	})
-	.controller('secondCtrl', function($scope) {
-		$scope.secondHello = function() {
-
-		}
-	})
-	.controller('siblingCtrl', function($scope) {
-		$scope.hi = "Sibling Controller";
 	});
 
 /***/ })
